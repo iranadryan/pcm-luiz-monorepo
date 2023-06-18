614bb0a8e19c1388a7a21f4a2e6e823f52c6aa24
Iran <contatoiranadryan@gmail.com>
2023-06-18 19:36:09 -0300

    fix api url

--- packages/client/src/lib/trpcClient.ts
+++ packages/client/src/lib/trpcClient.ts
@@ -2,8 +2,8 @@ import { createTRPCProxyClient, httpBatchLink } from '@trpc/client';
 import type { AppRouter } from 'server';
 import SuperJSON from 'superjson';
 
-export const trpcUrl = 'http://localhost:3001/trpc';
-// export const trpcUrl = 'https://api.sistemapcm.com/trpc';
+// export const trpcUrl = 'http://localhost:3001/trpc';
+export const trpcUrl = 'https://api.sistemapcm.com/trpc';
 // export const trpcUrl = 'http://3.22.99.134/trpc';
 
 export const trpcClient = createTRPCProxyClient<AppRouter>({
